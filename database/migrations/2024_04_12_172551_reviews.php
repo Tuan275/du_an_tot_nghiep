4edc56462492eb7ea565850d5faa8654cd6d7d44
longnt <longnt@ichiba.vn>
2024-04-13 00:37:41 +0700

    create table database

---
+++ database/migrations/2024_04_12_172551_reviews.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger('user_id');
+            $table->tinyInteger('service_id');
+            $table->tinyInteger('status')->default(1);
+            $table->string('name_service');
+            $table->longText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
