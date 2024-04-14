585bbad2caf2ee9e40ae35a1302b364c616be66c
longnt <longnt@ichiba.vn>
2024-04-14 21:44:55 +0700

    core project

---
+++ resources/views/client/home.blade.php
@@ -0,0 +1,8 @@
+@extends('client.layout.master')
+@section('content')
+<div class="about">
+  <div class="container">
+    
+  </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -8,11 +9,42 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/')
+    ->name('client.home');
+Route::middleware('checkadmin')->prefix('admin')->group(function () {
+    Route::get('index', [UserController::class, 'index'])
+        ->name('admin.index');
+    Route::prefix('user')->group(function () {
+        Route::get('list', [UserController::class, 'index'])
+            ->name('admin.user.list');
+        Route::get('create', [UserController::class, 'create'])
+            ->name('admin.user.create');
+        Route::post('store', [UserController::class, 'store'])
+            ->name('admin.user.store');
+    });
 });
+
+Route::prefix(('user'))->group(function () {
+    Route::get('login_form', [UserController::class, 'login_form'])
+        ->name('user.login_form');
+    Route::post('login', [UserController::class, 'login'])
+        ->name('user.login');
+    Route::get('register', [UserController::class, 'register'])
+        ->name('user.register');
+    Route::post('store', [UserController::class, 'store'])
+        ->name('user.store');
+    Route::get('logout', [UserController::class, 'logout'])
+        ->name('user.logout');
+});
+
+Route::prefix(('client'))->group(function () {
+    Route::get('/')
+        ->name('client.home');
+});
+
+// Route::get('send_mail/{id}', [SendMailController::class, 'send_mail']);
