cdb3ba74de2ed533d47b3bccec3a605d856f191f
TuanTT <tranthanhtuann275@gmail.com>
2024-05-06 22:02:09 +0700

    slide

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -1,10 +1,11 @@
 @extends('client.layout.master')
 @section('content')
+
 <div class="grid">
   @foreach ( $ser as $items)
   <div class="py-5 bg-[#333]">
     <div class="container flex gap-5 items-center">
-      <img width="100" class="w-[500px] h-[300px]" src="../../../{{$items->image}}" alt="">
+      <img width="100" class="w-[30%] h-[30%]" src="../../../{{$items->image}}" alt="">
       <div class="grid gap-2 text-white">
         <p class="text-2xl font-bold">{{$items->name_service}}</p>
         <p class="text-base">{{$items->description}}</p>
---
+++ resources/views/client/layout/footer.blade.php
@@ -0,0 +1,28 @@
+<!-- <section class="app-container">
+        <p>Liên hệ với Hải Kẹo Studio</p>
+        <input type="text" placeholder="Nhập email của bạn...">
+        <button class="py-1 px-3 border border-[#ccc] bg-gray hover:opacity-70">
+        SEND 
+        </button>
+    </section> -->
+<!-- -----------------------------footer------------------------------- -->
+    <div class="footer-top">
+         <li><a href="">Liên hệ</a></li>
+         <li><a href="">Tuyển dụng</a></li>
+         <li><a href="">Giới thiệu</a></li>
+         <li>
+            <a href="" class="fab fa-facebook"></a>
+            <a href="" class="fab fa-tiktok"></a>
+            <a href="" class="fab fa-instagram"></a>
+        </li>
+    </div>
+    <div class="footer-center">
+        <i class="fas fa-map-marker-alt"></i>
+        Cơ sở 1: xã Phú Lương, huyện Đông Hưng, tỉnh Thái Bình, Việt Nam<br>
+        <i class="fas fa-map-marker-alt"></i>
+        Cơ sở 2: xã Thạch Hòa, huyện Thạch Thất, TP.Hà Nội, Việt Nam<br>
+        Điện thoại: <b>08.86.86.88.36</b> 
+    </div>
+    <div class="footer-bottom">
+        <p>© Hải Kẹo Studio</p>
+    </div>
